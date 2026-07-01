07d6f67b49a12a0ab939f526f77a330c70df23fb
yanwenjing <yan.wenjing@trans-cosmos.com.cn>
2026-07-01 09:48:10 +0800

    upd

--- react-ud/src/Menu/Menu.tsx
+++ react-ud/src/Menu/Menu.tsx
@@ -234,11 +234,13 @@ const Menu: React.FC = () => {
     <div className='navigation-menu'>
       {/* 标题区域 */}
       <div className='menu-header'>
-        <h1>Generate Document</h1>
+        <h1>VOLVO</h1>
       </div>
 
       {/* 菜单项列表 */}
       <ul className='menu-list'>
+        {/* 副标题：Generate Document */}
+        <div className='menu-subtitle'>Generate Document</div>
         {menuData.map((module) => (
           <li key={module.id} className='menu-group'>
             <div className='group-title'>{module.label}</div>
@@ -251,7 +253,7 @@ const Menu: React.FC = () => {
                     className={`menu-item ${isLink ? "clickable" : "disabled"}`}
                     onClick={() => handleItemClick(item.path)}
                   >
-                    <span className='arrow-icon'>{isLink ? "»" : "-"}</span>
+                    <span className='arrow-icon'>»</span>
                     <span className='item-label'>{item.label}</span>
                   </li>
                 );
